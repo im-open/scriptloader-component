889fc322246d111c0cc487ccba2412d1365d5b0b
Steve Matney <steve.matney@extendhealth.com>
2020-11-30 15:30:29 -0700

    Build: adding semantic release configuration

---
+++ release.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  repositoryUrl: "git@github.com:WTW-IM/scriptloader-component.git",
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "eslint",
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/github",
+      {
+        assets: "*.tgz",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        message:
+          "Docs: ${nextRelease.version} [skip ci]\n\n${nextRelease.note}",
+      },
+    ],
+  ],
+};
