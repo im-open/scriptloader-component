86340541a4624394359fae46dd69a7a390dec7b5
Steve Matney <steve.matney@extendhealth.com>
2020-12-02 11:10:41 -0700

    Update: renaming getScriptUpdaters to getCachedScriptUpdaters

--- src/scriptCache.ts
+++ src/scriptCache.ts
@@ -41,7 +41,7 @@ Object.defineProperty(window, "__loadedScriptsUpdaters", {
       window.__loadedScriptsUpdatersInternal || {}),
 });
 
-const getScriptUpdaters = (url: string): CachedScriptUpdater[] =>
+const getCahcedScriptUpdaters = (url: string): CachedScriptUpdater[] =>
   (window.__loadedScriptsUpdaters[url] =
     window.__loadedScriptsUpdaters[url] || []);
 
@@ -63,7 +63,7 @@ export function updateCachedScript(
     ...updatedScript,
   });
 
-  getScriptUpdaters(url).forEach((updater) => updater(newScript));
+  getCahcedScriptUpdaters(url).forEach((updater) => updater(newScript));
 
   return newScript;
 }
@@ -72,14 +72,17 @@ export function addScriptUpdater(
   url: string,
   updater: CachedScriptUpdater
 ): void {
-  window.__loadedScriptsUpdaters[url] = [...getScriptUpdaters(url), updater];
+  window.__loadedScriptsUpdaters[url] = [
+    ...getCahcedScriptUpdaters(url),
+    updater,
+  ];
 }
 
 export function removeScriptUpdater(
   url: string,
   updater: CachedScriptUpdater
 ): void {
-  window.__loadedScriptsUpdaters[url] = getScriptUpdaters(url).filter(
+  window.__loadedScriptsUpdaters[url] = getCahcedScriptUpdaters(url).filter(
     (currentUpdater) => currentUpdater !== updater
   );
 }
