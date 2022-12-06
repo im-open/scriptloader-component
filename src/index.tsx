da990af969f6714660ce331355cb8674311bab30
Steve Matney <steve.matney@extendhealth.com>
2022-12-06 10:29:45 -0700

    Fix: shortening default export syntax

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-export { default as default } from "./ScriptLoader";
+export { default } from "./ScriptLoader";
 export * from "./ScriptLoader";
 export { default as useScriptLoader } from "./hooks/useScriptLoader";
 export * from "./hooks/useScriptLoader";
